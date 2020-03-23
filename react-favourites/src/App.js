a7e4c5155d2bafd6827486407acf2b1dd53eb2ce
Damian Ospara <damian89m@gmail.com>
2020-03-23 21:18:28 +0100

    Extract global styles

--- react-favourites/src/App.js
+++ react-favourites/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
-import variables from "./variables";
-import "bulma/css/bulma.min.css";
 
 // Components
-import Home from "./components/Home";
 import FilmDetails from "./components/FilmDetails";
-import Header from "./components/Header";
 import FilmList from "./components/FilmList";
+import GlobalStyle from './GlobalStyles';
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -18,14 +16,8 @@ const App = () => {
         <Header></Header>
         <Switch>
           <Route path="/" exact strict component={Home}></Route>
-          <Route path="/film-list" exact strict component={FilmList}></Route>
-          <Route
-            path="/film-list/:id"
-            exact
-            strict
-            component={FilmDetails}
-          ></Route>
-
+          <Route path="/films" exact strict component={FilmList}></Route>
+          <Route path="/films/:id" exact strict component={FilmDetails}></Route>
           <Redirect to="/" />
         </Switch>
       </BrowserRouter>
@@ -34,46 +26,3 @@ const App = () => {
 };
 
 export default App;
-
-// Styles
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Baloo+Da+2:400,500,700&display=swap');
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;     
-  }
-  html {
-    font-size: 10px;
-  }
-  body {
-    background-color: ${variables.mainColor};
-    color: ${variables.fontColor};
-    font-family: ${variables.mainFont};
-    font-weight: 400;
-    line-height: 1.7; 
-    min-height: 100vh;
-  } 
-  h1 {
-    font-size: 4rem
-  }
-  h2 {
-    font-size: 3.2rem;
-  }
-  h3 {
-    font-size: 2.4rem
-  }
-  div {
-    font-size: 1.6rem
-  }
-  label.label {
-    color: ${variables.fontColor};
-    font-size: 2rem;
-  }
-  .checkbox {
-    font-size: 2rem;
-    &:hover {
-      color: ${variables.activeColor}
-    }
-  }
-`;
